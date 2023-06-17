bb651c72edfa4535031784e5cf066c7145d8c1a8
Matthew Gonzalez <109435182+Matthew-Steven-Gonzalez@users.noreply.github.com>
2023-06-17 13:24:15 -0400

    Furthered Study

--- Program.cs
+++ Program.cs
@@ -21,7 +21,6 @@ namespace HelloWorld
             Console.WriteLine("num is " + num1);
             Console.WriteLine("Num1(" + num1 + ") plus num2(" + num2 + ") is " + sum);
 
-
             //ints
             
             int num3, num4, num5;
@@ -63,8 +62,82 @@ namespace HelloWorld
 
             Console.WriteLine("{0} / {1} = {2}", dd1, dd2, ddDiv);
 
+            // strings
+
+            string myname = "Matthew";
+
+            string message = "My name is " + myname + ".";
+
+            //string methods
+
+            string capsMessage = message.ToUpper();
+            string lowerMessage = message.ToLower();
+
+            Console.WriteLine("My name is {0}.",myname);
+            Console.WriteLine(message);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowerMessage);
+            Console.ReadLine();
 
 
+            //console methods 
+
+            //Write
+
+            Console.Write(message);
+
+            //WriteLine
+
+            Console.WriteLine(message);
+
+            //ReadLine
+
+            int stringValue = Console.Read();
+            Console.WriteLine(stringValue);
+
+
+            //implcit conversion
+            int num = 123344353;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+
+            //explicit conversion
+            //cast double to int
+            double mydouble = 32.42;
+            int myInt;
+            myInt = (int)mydouble;
+
+            //type conversion
+
+            string myString = myFloat.ToString();
+
+            Console.WriteLine(myString);
+
+            string myFirstString = "15";
+            string mySecondString = "13";
+            int num7 = int.Parse(myFirstString);
+            int num8 = int.Parse(mySecondString);
+            int intResult = num7 + num8;
+            string result = myFirstString + mySecondString;
+
+            Console.WriteLine(result);
+            Console.WriteLine(intResult);
+
+            //Assingment 1 
+
+            string stringForFloat = "0.85";
+            string stringForInt = "12345";
+
+            int intValue = int.Parse(stringForInt);
+            float floatValue = float.Parse(stringForFloat);
+
+            Console.WriteLine(floatValue.GetType());
+            Console.WriteLine(stringValue.GetType());
+            Console.WriteLine(stringForFloat.GetType());
+            Console.WriteLine(stringForInt.GetType());
         }
     }
 }
