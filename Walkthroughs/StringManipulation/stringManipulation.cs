531030fee844baef1395140f78092ea895acf70e
Matthew Gonzalez <109435182+Matthew-Steven-Gonzalez@users.noreply.github.com>
2023-06-26 15:43:34 -0400

    test

---
+++ Walkthroughs/StringManipulation/stringManipulation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Walkthroughs
+{
+	public class Class1
+	{
+		static void Main(string[] args)
+		{
+			//define Variables
+
+			int age = 32;
+			string name = "Freddie";
+
+			//1.String concatination
+			Console.WriteLine("String Concatenation:");
+			Console.WriteLine("Hello my name is " + name + " and I am " + age + " years old.");
+		}
+	}
+}
+
