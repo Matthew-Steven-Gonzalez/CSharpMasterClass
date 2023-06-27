2856c2f216aa56dd221a17566a2dc0bbb33cb2ac
Matthew Gonzalez <109435182+Matthew-Steven-Gonzalez@users.noreply.github.com>
2023-06-27 18:24:17 -0400

    june 27 2023

--- Walkthroughs/Methods/Program.cs
+++ Walkthroughs/Methods/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        const string friend1 = "Tony";
+        const string friend2 = "Greg";
+        const string friend3 = "Teri";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+            Console.ReadLine();
+
+            int num1 = 5;
+            int num2 = 0;
+            int result;
+
+            try 
+            {
+                result = num1 / num2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
         }
+
+        public static void GreetFriend(string friend) 
+        {
+            Console.WriteLine("Hello " + friend + "!");
+        }
+
+   
     }
 }
