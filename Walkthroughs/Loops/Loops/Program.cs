4e6ac1a9ee2538e835e1ab70970c8a54cc659ae5
gonzalez.matthew.steven@gmail.com <gonzalez.matthew.steven@gmail.com>
2023-07-04 16:13:52 -0400

    Completed loops 1 challenge and created grade app.

---
+++ Walkthroughs/Loops/Loops/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        public static int average;
+        public static int total = 0;
+        public static int grade = 0;
+        public static int counter = 0;
+
+        public 
+        static void Main(string[] args)
+        {
+            //Challenge build a for loop that prints odd numbers
+
+            for(int i = 1; i <= 20;i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //loops 1 challenge
+
+            Console.WriteLine("Welcome to average");
+            Console.WriteLine("------------------");
+            while (grade != -1)
+            {
+                Console.WriteLine("Enter a grade between 0-20");
+                Console.WriteLine("Please enter grade: ");
+                if (Int32.TryParse(Console.ReadLine(), out grade))
+                {
+                    if(grade == -1)
+                    {
+                        continue;
+                    }
+
+                    if(grade <= 20  && grade >= 0 )
+                    {
+                        total += grade;
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number is outside the parameters.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You entered a non numerical value.");
+                }
+            }
+            try
+            {
+                Console.WriteLine("The average is {0}", average = total / counter);
+
+            }catch (Exception)
+            {
+                Console.WriteLine("you entered no numbers");
+            }
+
+        }
+
+    }
+}
