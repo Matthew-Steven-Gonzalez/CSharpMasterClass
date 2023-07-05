593659f96ac91e548c1612d9a532d81e49d9e0b9
gonzalez.matthew.steven@gmail.com <gonzalez.matthew.steven@gmail.com>
2023-07-05 15:45:58 -0400

    begun OOP lesson

---
+++ Walkthroughs/OOP/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Car
+    {
+        public Car() 
+        {
+            Console.Write("Car was created.");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving.");
+        }
+    }
+
+
+}
---
+++ Walkthroughs/OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car();
+            audi.Drive();
+        }
+    }
+}
